7ddc3b4145dd1a6c477f50ccac974ee473e481e3
nanonino <mugilan@nanonino.in>
2025-07-16 13:08:57 +0530

    chnages-vercel-01

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,13 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Enable CORS before app.listen
+  // ✅ Allow all origins temporarily
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true, // if you are using cookies or headers
+    origin: '*',
   });
 
-  await app.listen(process.env.PORT ?? 8080);
-  console.log(`✅ Server running on http://localhost:${process.env.PORT}`);
+  await app.listen(process.env.PORT || 8080);
+  console.log(`✅ Server running on port ${process.env.PORT || 8080}`);
 }
 bootstrap();
